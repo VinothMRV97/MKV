c369666a2b1d3c0b65f5c3cf0eb37b0ceb2bf962
VinothMRV97 <30317766+VinothMRV97@users.noreply.github.com>
2017-07-23 20:12:45 +0530

    Create seq.c

---
+++ seq.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+int i,a,b,flag,c;
+scanf("%d%d",&a,&b);
+while(a<=b)
+{
+flag=0;
+for(i=2;i<=a/2;i++)
+{
+if(a%i==0)
+{
+flag=1;
+break;
+}
+}
+if(flag==0)
+{
+printf("%d\t",a);
+c++;
+}
+a++;
+}
+printf("%d",c);
+return 0;
+}
