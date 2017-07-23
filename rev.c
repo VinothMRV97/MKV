e6427afb5b11ed103082ef9aefcc19a011762e3a
VinothMRV97 <30317766+VinothMRV97@users.noreply.github.com>
2017-07-23 19:32:37 +0530

    Update rev.c

--- rev.c
+++ rev.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-int n,i,o,s,r=1;
+int n,s,r=0;
 scanf("%d",&n);
-o=n;
-for(i=2;i<=n/2;i++)
+while(n!=0)
 {
 s=n%10;
 r=r*10+s;
 n/=10;
 }
-printf("%d",o);
+printf("%d",r);
+  return 0;
 }
