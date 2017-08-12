25e830a12fdb4f644c7dd9e923722e1dd757483d
VinothMRV97 <30317766+VinothMRV97@users.noreply.github.com>
2017-08-12 16:48:19 +0530

    Update words.c

--- words.c
+++ words.c
@@ -1,15 +1,25 @@
 #include<stdio.h>
 int main()
 {
-int i,w=0;
+int a=0,c=0,n=0,i;
 char s[100];
 scanf("%s",&s);
-for(i=0;s[i]!="/0";i++)
+for(i=0;s[i]!='\0';i++)
 {
-if(s[i]>='a' && s[i]<='z' || s[i]>='A' && s[i]<='Z')
+if(s[i]>='a' && s[i]<='z'||s[i]>='A' &&s[i]<='z')
 {
-w++;
+a++;
 }
-printf("%d",w);
+else if(s[i]>='0' && s[i]<='9')
+{
+n++;
+}
+else
+{
+c++;
+}
+}
+printf("characters=%d",c);
+printf("\nnumber=%d",n);
+printf("\nAlphabets=%d",a);
 return 0;
-}
