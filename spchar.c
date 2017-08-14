0997f6b4c41b2e8170c3efc25aa42e334dfed67c
VinothMRV97 <30317766+VinothMRV97@users.noreply.github.com>
2017-08-14 13:12:34 +0530

    Create spchar.c

---
+++ spchar.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+int a=0,c=0,n=0,i;
+char s[100];
+scanf("%s",&s);
+for(i=0;s[i]!='\0';i++)
+{
+if(s[i]>='a' && s[i]<='z'||s[i]>='A' &&s[i]<='z')
+{
+a++;
+}
+else if(s[i]>='0' && s[i]<='9')
+{
+n++;
+}
+else
+{
+c++;
+}
+}
+printf("spcharacters=%d",c);
+return 0;
+}
